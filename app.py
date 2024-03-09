1ba181f31487b10b189fe233f3cca52cf9a2b570
alirezax2 <alireza.naziri@gmail.com>
2024-03-09 03:59:03 +0100

    add interactive candle stick chart

--- app.py
+++ app.py
@@ -10,6 +10,39 @@ import hvplot.pandas
 
 pn.extension('bokeh', template='bootstrap')
 
+
+def _extract_raw_data(ticker="MSFT", period="6mo", interval="1d"):
+  import yfinance as yf
+  df = yf.Ticker(ticker)
+  return df.history(period=period, interval=interval).reset_index()
+
+def _transform_data(raw_data: pd.DataFrame):
+  from datetime import timedelta
+  data = raw_data[["Date", "Open", "High", "Low", "Close", "Volume"]].copy(deep=True).rename(columns={
+      "Date": "time",
+      "Open": "open",
+      "High": "high",
+      "Low": "low",
+      "Close": "close",
+      "Volume": "volume",
+  })
+  t_delta = timedelta(hours=1)
+  data['time_start'] = data.time - 9*t_delta # rectangles start
+  data['time_end'] = data.time + 9*t_delta    # rectangles end
+  data['positive'] = ((data.close - data.open)>0).astype(int)
+  return data
+
+def make_candle_stick(ticker):
+    raw_data = _extract_raw_data(ticker = ticker)
+    data = _transform_data(raw_data=raw_data)
+    _delta = np.median(np.diff(data.time))
+    candlestick = hv.Segments(data, kdims=['time', 'low', 'time', 'high']) * hv.Rectangles(data, kdims=['time_start','open', 'time_end', 'close'], vdims=['positive'])
+    candlestick = candlestick.redim.label(Low='Values')
+    candlechart = pn.Column(candlestick.opts(hv.opts.Rectangles(color='positive', cmap=['red', 'green'], responsive=True), hv.opts.Segments(color='black', height=400, responsive=True , show_grid=True)) , 
+                     data.hvplot(x="time", y="volume", kind="line", responsive=True, height=200).opts( show_grid=True) )
+                    #  data.hvplot(y="volume", kind="bar", responsive=True, height=200) )
+    return candlechart
+
 #Rading gurufocus from github action pipeline
 current_datetime = datetime.now().strftime("%Y-%m-%d")    
 daily_gurufocus_csvfile = f"https://raw.githubusercontent.com/alirezax2/GurusFocusCrawl/main/gurufocus/GuruFocus_merged_{current_datetime}.csv"
@@ -54,7 +87,9 @@ MarketCap = pn.widgets.FloatSlider(name='Market Capital (B$)', start=0, end=4000
 
 def get_DF(DF,ticker,SmartScore,GFValuepercent, FinVizTargetpercent, Sector,MarketCap):
   if ticker and ticker!="ALL":
-    return pn.widgets.Tabulator(DF.query("Ticker == @ticker"), height=800, widths=200, show_index=False)
+    table1 = pn.widgets.Tabulator(DF.query("Ticker == @ticker"), height=800, widths=200, show_index=False)
+    chart1 = make_candle_stick(ticker)
+    return pn.Column(table1,chart1)
   else:
     return pn.widgets.Tabulator( DF.query("SmartScore>=@SmartScore[0] & SmartScore <= @SmartScore[1] & GFValuepercent>=@GFValuepercent & FinVizTargetpercent>@FinVizTargetpercent & Sector in @Sector & MarketCap>@MarketCap"), height=800, widths=200, show_index=False)
 
